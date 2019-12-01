27c3eae47f318f01a795efeba92352088e71eaf8
paul <paul.genho@gmail.com>
2019-12-01 08:49:01 -0700

    add part 2 to day1

--- day1/solve.py
+++ day1/solve.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import math
 
-weights = open('input.txt', 'r').readlines()
+masses = open('input.txt', 'r').readlines()
 
-solution = sum([((int(i.strip()) / 3) - 2) for i in weights]) 
 
-print(solution)
+def part1():
+    return sum([((int(i.strip()) / 3) - 2) for i in masses]) 
+
+
+def part2():
+    total = 0
+
+    for m in masses:
+        m = int(m.strip())
+
+        requires = math.floor((m / 3) - 2)
+        total += requires
+
+        while requires > 0:
+            requires = math.floor((requires / 3) - 2)
+
+            if requires > 0:
+                total += requires
+
+
+    return total
+
+
+print(part1())
+print(part2())
